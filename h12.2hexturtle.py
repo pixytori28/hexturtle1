014a2552a9f2aa57f0a1818ff0edf382b96337d5
pixytori28 <victoria.inahuazo11@myhunter.cuny.edu>
2024-03-05 00:25:55 -0500

    Add files via upload
    alternate version (recommended by a TA from lab review)

---
+++ h12.2hexturtle.py
@@ -0,0 +1,18 @@
+#name:Victoria Inahuazo
+#email:victoria.inahuazo11@myhunter.cuny.edu
+#date: 03/05/24
+#the following program generates a turtle with hexcode
+#alternative to the original version
+
+import turtle
+wn = turtle.Screen()
+
+donnie = turtle.Turtle()
+
+turtle.colormode(255)
+donnie = input("Enter color:")
+donnie.shape("turtle")
+donnie.color("#A922A9")
+
+
+wn.exitonclick()  
